390a1de435f9da32ba5f64a909b97909e87cb40d
drago <119659334+qithoniq@users.noreply.github.com>
2023-08-12 02:32:58 +0300

    Update فحص.py

--- drago/plugins/فحص.py
+++ drago/plugins/فحص.py
@@ -51,7 +51,7 @@ async def amireallyalive(event):
     _, check_sgnirts = check_data_base_heal_th()
     EMOJI = gvarstatus("ALIVE_EMOJI") or "⿻┊‌‎"
     ALIVE_TEXT = gvarstatus("ALIVE_TEXT") or "**父[ 𝙳𝚁𝙰𝙶𝙾 𝙸𝚂 𝚆𝙾𝚁𝙺𝙸𝙽𝙶 ](t.me/src_dra)父**"
-    DRAGO_IMG = gvarstatus("ALIVE_PIC") or Config.A_PIC or "https://telegra.ph/file/f4c895df55defd2875794.mp4"
+    DRAGO_IMG = gvarstatus("ALIVE_PIC") or Config.A_PIC or "https://telegra.ph/file/40672f276c69564208680.jpg"
     dragoiq_caption = gvarstatus("ALIVE_TEMPLATE") or temp
     caption = dragoiq_caption.format(
         ALIVE_TEXT=ALIVE_TEXT,
