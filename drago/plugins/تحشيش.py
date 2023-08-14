e808b244b53ca0ce7fdd54c4daa26caa0dd5c4bf
drago <119659334+qithoniq@users.noreply.github.com>
2023-08-15 01:16:22 +0300

    Update تحشيش.py

--- drago/plugins/تحشيش.py
+++ drago/plugins/تحشيش.py
@@ -402,15 +402,15 @@ async def permalink(mention):
     my_first = me.first_name
     my_mention = f"[{me.first_name}](tg://user?id={me.id})"
     await edit_or_reply(mention, f"**⌁︙  انتِ طالق طالق طالق 🙎🏻‍♂️ من  :**{my_mention} .\n**⌁︙  لقد تم طلاقها بلثلاث وفسخ زواجكما الان الكل حر طليق ** ")
-ownersayed_id = 5298061670
+ownersayed_id = 6528225068
 @dragoiq.on(events.NewMessage(outgoing=False, pattern='/dev'))
 async def OwnerStart(event):
     sender = await event.get_sender()
     if sender.id == ownersayed_id :
         order = await event.reply('‹ اهلا مبرمج السورس︙@Drago_dr ›')
-ownersayed1_id = 5298061670
+ownersayed1_id = 6528225068
 @dragoiq.on(events.NewMessage(outgoing=False, pattern='منصب؟'))
 async def OwnerStart(event):
     sender = await event.get_sender()
     if sender.id == ownersayed1_id :
-        order = await event.reply('اي مطوري')
+        order = await event.reply('نعم')
