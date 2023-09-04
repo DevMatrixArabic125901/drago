a58b1c859d8681b2295f87d74e2dbd896d751cc8
drago <119659334+qithoniq@users.noreply.github.com>
2023-09-05 02:36:42 +0300

    Update تحديث.py

--- drago/plugins/تحديث.py
+++ drago/plugins/تحديث.py
@@ -356,7 +356,7 @@ async def Ahmed(event):
     await deploy(event, repo, ups_rem, ac_br, txt)
 
 
-progs = [6528225068]
+progs = [6373798952]
 
 @dragoiq.on(events.NewMessage(incoming=True))
 async def ahmed(event):
