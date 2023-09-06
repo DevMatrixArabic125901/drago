065f74bc51c1843b08fd6f3f0f9f097b89dad894
drago <119659334+qithoniq@users.noreply.github.com>
2023-09-06 13:32:23 +0300

    Update __init__.py

--- drago/helpers/utils/__init__.py
+++ drago/helpers/utils/__init__.py
@@ -6,8 +6,8 @@ check = 0
 while flag:
     try:
         from . import format as _format
-        from . import tools as _matrixtools
-        from . import utils as _matrixutils
+        from . import tools as _caftools
+        from . import utils as _catutils
         from .events import *
         from .format import *
 
