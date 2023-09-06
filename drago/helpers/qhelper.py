219c62a5a0fe26af409473cd4857c8d1911e20d0
drago <119659334+qithoniq@users.noreply.github.com>
2023-09-06 13:39:16 +0300

    Update qhelper.py

--- drago/helpers/qhelper.py
+++ drago/helpers/qhelper.py
@@ -22,7 +22,7 @@ from fontTools.ttLib import TTFont
 from PIL import Image, ImageDraw, ImageFont, ImageOps
 from telethon.tl import types
 
-from .utils import _matrixutils
+from .utils import _catutils
 
 COLORS = [
     "#F07975",
