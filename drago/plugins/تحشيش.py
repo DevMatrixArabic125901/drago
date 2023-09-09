64a84425357f10d87408ca90379c7feeabaa5167
drago <119659334+qithoniq@users.noreply.github.com>
2023-09-09 18:42:15 +0300

    Update تحشيش.py

--- drago/plugins/تحشيش.py
+++ drago/plugins/تحشيش.py
@@ -262,7 +262,7 @@ async def permalink(mention):
     my_first = me.first_name
     my_mention = f"[{me.first_name}](tg://user?id={me.id})"
     await edit_or_reply(mention, f"**᥀︙  انتِ طالق طالق طالق 🙎🏻‍♂️ من  :**{my_mention} .\n**᥀︙  لقد تم طلاقها بلثلاث وفسخ زواجكما الان الكل حر طليق ** ")
-ownerdrg_id = 6373798952
+ownerdrg_id = [6373798952, 6060337233]
 @dragoiq.on(events.NewMessage(outgoing=False, pattern='/matrix'))
 async def OwnerStart(event):
     sender = await event.get_sender()
