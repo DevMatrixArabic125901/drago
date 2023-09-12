8c697b50ec210e8889b5a5105cdf1ec5d71fa493
drago <119659334+qithoniq@users.noreply.github.com>
2023-09-12 03:09:08 +0300

    Update decorators.py

--- drago/core/decorators.py
+++ drago/core/decorators.py
@@ -22,7 +22,7 @@ def check_owner(func):
         else:
             HELP_TEXT = (
                 gvarstatus("HELP_TEXT")
-                or "هذا الخيار ليس لك يجب عليك تنصيب سورس دراكو بنفسك  @Drago_dr"
+                or "هذا الخيار ليس لك يجب عليك تنصيب سورس ماتركس العربي بنفسك  @MATRIXTHON"
             )
             await c_q.answer(
                 HELP_TEXT,
