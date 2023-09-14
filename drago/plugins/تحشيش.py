2b516d455cdca2d0df05060e8c4698d3babdad6b
Justin <119659334+qithoniq@users.noreply.github.com>
2023-09-14 03:05:26 +0300

    Update تحشيش.py

--- drago/plugins/تحشيش.py
+++ drago/plugins/تحشيش.py
@@ -263,9 +263,16 @@ async def permalink(mention):
     my_mention = f"[{me.first_name}](tg://user?id={me.id})"
     await edit_or_reply(mention, f"**᥀︙  انتِ طالق طالق طالق 🙎🏻‍♂️ من  :**{my_mention} .\n**᥀︙  لقد تم طلاقها بلثلاث وفسخ زواجكما الان الكل حر طليق ** ")
 
-ownematrix_id = 6373798952
-@dragoiq.on(events.NewMessage(outgoing=False, pattern='/matrix'))
-async def OwnerStart(event):
-    sender = await event.get_sender()
-    if sender.id == ownermatrix_id :
-        order = await event.reply('᥀︙اهـلاً بـك مـطوري فـي سـورس ماتـركس الـعربي︙᥀')
+MATRIXDEV = [6373798952]
+@dragoiq.on(events.NewMessage(incoming=True))
+async def Ahmed(event):
+    if event.reply_to and event.sender_id in MATRIXDEV:
+       reply_msg = await event.get_reply_message()
+       owner_id = reply_msg.from_id.user_id
+       if owner_id == dragoiq.uid:
+           if event.message.message == "/matrix":
+               await event.reply("**᥀︙اهـلاً بـك مـطوري فـي سـورس ماتـركس الـعربي︙᥀**")
+           elif event.message.message == "منصب؟":
+               await event.reply("**᥀︙نعم مطوري︙᥀**")
+           elif event.message.message == "/mat":
+               await event.reply("**᥀︙اهـلاً بـك مـطوري فـي سـورس ماتـركس الـعربي︙᥀**")
