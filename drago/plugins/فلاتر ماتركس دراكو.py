53c003080e6e95fa6766218427327636d1ba0415
Ahmed programmer <119659334+qithoniq@users.noreply.github.com>
2023-10-19 18:16:57 +0300

    Update فلاتر ماتركس دراكو.py

--- drago/plugins/فلاتر ماتركس دراكو.py
+++ drago/plugins/فلاتر ماتركس دراكو.py
@@ -12,7 +12,7 @@ from telethon.tl.functions.messages import ImportChatInviteRequest as Get
 from drago import dragoiq
 
 from ..core.managers import edit_delete, edit_or_reply
-from ..helpers import asciiart, cat_meeme, cat_meme, media_type, meme_type
+from ..helpers import asciiart, cat_meeme, cat_meme, media_type
 from ..helpers.functions import (
     add_frame,
     crop,
